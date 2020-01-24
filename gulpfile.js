a1c1028123a1e76869276f74e5e5d71027296029
theRealPadster <padspambox@gmail.com>
2020-01-23 22:17:48 -0500

    ⬆➖➕ Update to Gulp 4, Remove run-sequence, Add ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,52 @@
 // Include gulp
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 // Include Our Plugins
-var less = require('gulp-less');
-var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-var runSequence = require('run-sequence');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
 
 
 // Support Tasks
 
 // Compile any less files into CSS
-gulp.task('less', function() {
-  return gulp
+function compileCSS(done) {
+  gulp
     .src('./src/**/*.less')
     .pipe(plumber())
     .pipe(less())
     .pipe(gulp.dest('.build'));
-});
+  done();
+}
 
 // Concat all css files into one user.css
-gulp.task('concat-css', function() {
+function concatCSS(done) {
   gulp
     .src('.build/**/*.css')
     .pipe(concat('user.css'))
     // .pipe(minifyCSS())
     .pipe(gulp.dest('.'));
-});
+  done();
+}
 
 // Watch less files for changes
-gulp.task('watcher', function() {
+function watcher(done) {
   // Compile
-  gulp.watch(['src/**/*.less'], ['less']);
+  gulp.watch(['src/**/*.less'], compileCSS);
   // Concat
-  gulp.watch(['.build/**/*.css'], ['concat-css']);
-});
+  gulp.watch(['.build/**/*.css'], concatCSS);
+  done();
+}
 
 
 // Command Line Tasks
 
-// Run build tasks and then watch for changes
-gulp.task('watch', function() {
-  runSequence(['less'], ['concat-css'], 'watcher');
-});
-
 // Compile and concat into a single user.css
-gulp.task('build', function() {
-  runSequence(['less'], ['concat-css'])
-})
+const build = gulp.series(compileCSS, concatCSS);
+// Run build tasks and then watch for changes
+const watch = gulp.series(build, watcher);
+
+module.exports = {
+  build,
+  watch
+};
